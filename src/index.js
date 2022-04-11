31c470f2357d9c478b57a075c55bd3abfc6ca18d
taylorsabbag <38229747+taylorsabbag@users.noreply.github.com>
2022-04-11 14:44:29 -0400

    Added boilerplate routing code to index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import { store } from './app/store';
@@ -12,7 +13,9 @@ const root = createRoot(container)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 )
